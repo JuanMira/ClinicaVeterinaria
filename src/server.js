326a54755c4f31403a17624a3d4d077215ff740d
Juan Mira <juanmira20@hotmail.com>
2021-12-14 00:01:44 -0500

    server pet mc uri

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 module.exports = {
   auth_api_url: "https://veterinaria-auth-mcs.herokuapp.com/",
   customer_api_url: "https://mission-tic-account-ms.herokuapp.com/",
-  pets_api_url: "http://localhost:8080/",
+  pets_api_url: "https://mission-tic-pet-ms.herokuapp.com/",
 };
